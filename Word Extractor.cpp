92ebe670f5a5887d16ee547bbc5315c56186b6b2
Harshit Sharma <harshit744@gmail.com>
2017-11-05 23:49:00 +0530

    Word extractor is a program extracts words from sentence
    Word extractor first takes number of test cases as input from std in.
    Then , using Standard Template Library (STL) it prepares a vector of strings containing words of the sentence entered.

---
+++ Word Extractor.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int T;
+	cin>>T>>ws;
+	while(T)
+	{	
+		cout<<"Enter any String\n";
+		string s;
+		getline(cin,s);
+		vector<string> word;
+		int start = 0 , length = 0 ;
+		for( int i = 0 ; i < s.size() ; i++ )
+		{
+			if( s[i] == 32 )
+			{
+				word.push_back(s.substr(start,length));
+				start = i + 1 ;
+				length = 0 ;
+			}
+			else
+			{
+				length++;
+			}
+		}
+        for(int i = 0 ; i < word.size(); i++)
+        {
+            cout<<word[i]<<endl;
+        }
+        T--;
+	}
+}
